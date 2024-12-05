6b4532917f080ba8667b57aaeb7f8dc63401a82e
Pedro Geovani <pedro.geovanisc@gmail.com>
2024-12-05 14:12:22 -0300

    [Frontend] create projet

---
+++ portifolio/src/App.vue
@@ -0,0 +1,10 @@
+<script lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>  
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>  
+  <RouterView />
+</template>
